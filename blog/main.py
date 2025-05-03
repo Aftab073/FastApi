8326951cfc5b4c36453b2134616b1d3bd55fc464
Aftab073 <tamboliaftab84@gmail.com>
2025-05-03 16:17:47 +0530

    update main file

--- blog/main.py
+++ blog/main.py
@@ -1,54 +1,12 @@
-from fastapi import FastAPI, Depends
-from . import schemas, models
+from fastapi import FastAPI
+from ..app.blog import  models
 from .database import engine
-from sqlalchemy.orm import Session
-from passlib.context import CryptContext
+from  .routers import blog, user, authentication
+
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-@app.post('/blog')
-def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
-
-@app.get('/blog/{id}')
-def delete_blog(id: int, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        return {"error": "Blog not found"}
-    db.delete(blog)
-    db.commit()
-    return {"message": "Blog deleted successfully"}
-
-
-    
-@app.get('/blog')
-def get_blogs(id, db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).filter(models.Blog.id == id).first()
-    return blogs
-
-@app.put('/blog/{id}')
-def update_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        return {"error": "Blog not found"}
-    blog.title = request.title
-    blog.body = request.body
-    db.commit()
-    db.refresh(blog)
-    return blog
-
-pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated= 'auto')
-
-@app.post('/user')
-def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    hashedPassword = pwd_cxt.hash(request.password)
-    new_user = models.User(name=request.name, email=request.email, password=hashedPassword )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
